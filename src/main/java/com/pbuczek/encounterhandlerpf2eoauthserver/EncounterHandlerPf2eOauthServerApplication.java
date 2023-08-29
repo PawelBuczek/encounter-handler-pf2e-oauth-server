d345c7c781551e131f197c6edfd8bb91e64f6eb3
PawelBuczek <pawel.buczek@email.com>
2023-08-29 23:27:58 +0200

    Initial commit

---
+++ src/main/java/com/pbuczek/encounterhandlerpf2eoauthserver/EncounterHandlerPf2eOauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.pbuczek.encounterhandlerpf2eoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncounterHandlerPf2eOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncounterHandlerPf2eOauthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbuczek/encounterhandlerpf2eoauthserver/EncounterHandlerPf2eOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbuczek.encounterhandlerpf2eoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncounterHandlerPf2eOauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

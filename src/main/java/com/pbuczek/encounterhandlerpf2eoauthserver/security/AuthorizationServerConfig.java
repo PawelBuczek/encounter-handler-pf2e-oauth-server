ed8381bfe53ba291a468ee73f1fc9d84c81346d7
PawelBuczek <pawel.buczek@email.com>
2023-08-30 09:10:35 +0200

    added authServerSecurityFilterChain

--- src/main/java/com/pbuczek/encounterhandlerpf2eoauthserver/security/AuthorizationServerConfig.java
+++ src/main/java/com/pbuczek/encounterhandlerpf2eoauthserver/security/AuthorizationServerConfig.java
@@ -3,6 +3,10 @@ package com.pbuczek.encounterhandlerpf2eoauthserver.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -10,6 +14,7 @@ import org.springframework.security.oauth2.server.authorization.client.InMemoryR
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.UUID;
 
@@ -30,5 +35,12 @@ public class AuthorizationServerConfig {
                 .scope("articles.read")
                 .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.formLogin(Customizer.withDefaults()).build();
     }
 }
